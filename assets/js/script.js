481f3027e75fec0ffd2894406c8dc7f7e9024393
KonradJam <acidburn.pl@gmail.com>
2025-03-25 19:46:29 +0100

    Remove unused design files and update social sharing functionality for responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,39 @@ const socials = document.getElementById('socials');
 const isDesktop = () => window.innerWidth > 768;
 
 
-isDesktop() ? socials.classList.add('socials--desktop') : socials.classList.remove('socials--desktop');
+const updateSocialsClass = () => {
+    socials.classList.remove('socials--desktop', 'socials--mobile');
+    isDesktop() ? socials.classList.add('socials--desktop') : socials.classList.add('socials--mobile'); 
+}
+updateSocialsClass();
 
 articlePreviewShareBtn.addEventListener('click', () => {
     isDesktop() ? socials.classList.toggle('socials--show-desktop') : socials.classList.toggle('socials--show-mobile');
 });
 
+
+let resizeTimer;
 window.addEventListener('resize', () => {
-    isDesktop() ? socials.classList.add('socials--desktop') : socials.classList.remove('socials--desktop');
+    const isCurrentVisable = socials.classList.contains('socials--show-desktop') || socials.classList.contains('socials--show-mobile');
+
+    if (!isCurrentVisable) {
+        socials.style.transition = "none";
+    }
+
+    clearTimeout(resizeTimer);
+    setTimeout(() => {
+        socials.style.transition = '';
+    }, 50);
+
+    updateSocialsClass();
+
+    if (isCurrentVisable) {
+        if (isDesktop()) {
+            socials.classList.remove('socials--show-mobile');
+            socials.classList.add('socials--show-desktop') 
+        } else {
+            socials.classList.remove('socials--show-desktop');
+            socials.classList.add('socials--show-mobile');
+        }
+    }
 });
